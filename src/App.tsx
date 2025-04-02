1158af64fd847606b12a7171044b9442dde2b417
DESKTOP-PGDNCDH\User <sanotos49@gmail.com>
2025-04-01 22:15:12 -0300

    inicio da edição do aplicativo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
-function App() {
+import { Box, Card, Typography } from '@mui/material';
+
+export default function App() {
 
   return (
-      <div>
-        <p>
-          TESTE
-        </p>
-      </div>
+        <Box
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={'flex-start'}
+          width={'100vw'}
+          height={'100vh'}
+        >
+          <Card
+          sx={{
+            display: 'flex',
+            direction: 'column',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            width: '100%',
+            marginTop: '144px',
+            marginX: '16px'
+          }}
+          >
+
+            {/* titulo */}
+            <Box
+              marginY={'16px'}
+            >
+              <Typography variant='h5' fontWeight={700}>
+              calculadora imc
+              </Typography>
+            </Box>
+
+            {/* entrada */ }
+            <Box>
+              <Typography variant='h5'>
+                altura
+              </Typography>
+            </Box>
+
+            {/* saida */}
+            <Box>
+              resultado
+            </Box>
+          </Card>
+        </Box>
   )
 }
-
-export default App
